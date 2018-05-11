bdd540502e87debac8d875026eacf01fb096a4d4
logonrm <logonrm@fiap.com.br>
2018-05-11 09:42:17 -0300

    Initial commit

---
+++ app/src/main/java/com/example/logonrm/cadastrojogoaula1105/CadastroActivity.kt
@@ -0,0 +1,40 @@
+package com.example.logonrm.cadastrojogoaula1105
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_cadastro.*
+
+class CadastroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+
+        btLimpar.setOnClickListener{
+            limpar()
+        }
+
+        btCadastrar.setOnClickListener {
+            val telaSucesso = Intent(this, SucessoCadastroActivity::class.java)
+            //telaSucesso.putExtra("nome", etNome.text.toString())
+
+            val jogo = Jogo(
+                etNome.text.toString(),
+                etGenero.text.toString(),
+                etAno.text.toString().toInt()
+                )
+
+            telaSucesso.putExtra("jogo", jogo)
+            startActivity(telaSucesso)
+            limpar()
+        }
+    }
+
+    fun limpar(){
+        etNome.setText("")
+        etAno.setText("")
+        etGenero.setText("")
+    }
+
+}
